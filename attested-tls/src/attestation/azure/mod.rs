3a3f2186fa4c7cf96e59846f5433e30d31504dc4
peg <ameba23@systemli.org>
2026-02-18 13:08:05 +0100

    Bump timestamp in test

--- attested-tls/src/attestation/azure/mod.rs
+++ attested-tls/src/attestation/azure/mod.rs
@@ -341,7 +341,7 @@ mod tests {
 
         // To avoid this test stopping working when the certificate is no longer valid we pass in a
         // timestamp
-        let now = 1764621240;
+        let now = 1771416451;
 
         let measurements_json = br#"
         [{
